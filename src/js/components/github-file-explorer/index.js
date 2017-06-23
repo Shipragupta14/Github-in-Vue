bdc0876d2e096c6ab3cfb05334a24f59043aee58
Shipragupta14 <shipragupta1411@gmail.com>
2017-06-23 13:41:43 +0530

    Added highlighter to the README.md

--- src/js/components/github-file-explorer/index.js
+++ src/js/components/github-file-explorer/index.js
@@ -57,24 +57,18 @@
             },
 
             getRead: function(){
-                this.loading =true;
                 var r = this;
-                var string;
-                var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}};
-                this.$http.get('https://api.github.com/repos/' + this.fullRepoUrl + '/contents/README.md?ref=master' )
+                this.$http.get('https://raw.githubusercontent.com/'+ this.fullRepoUrl +'/master/README.md' )
                 .then(function(data){ 
                     r.read=data.data ;
-                    string = r.read.content;
-                    var markdownContent =Base64.decode(string);
-                    var htmlContent = markdown.toHTML(markdownContent);
+                    var htmlContent = marked(r.read, { sanitize: true , gfm : true});;
                     var toReplaceWith ="src=\"https://raw.githubusercontent.com/"+ r.fullRepoUrl + "/master/";
                     r.display = htmlContent.replace(/src="[^http]/g, toReplaceWith);
-                    r.xhr={};
-                    r.loading = false;   
+                    r.xhr={}; 
+
                 }).catch(function(err){
                     r.xhr=err;
-                    r.info = {};
-                    r.loading = false;   
+                    r.info = {}; 
 
                 });
             },
@@ -89,10 +83,11 @@
             },
             goBack: function() {
                 this.path = this.path.split('/').slice(0, -1).join('/');
-                if (this.path === '') this.path = '/';
+                if (this.path === ''){
+                 this.path = '/'
                 this.getFiles();
                 this.getRead();
-
+           }
             },
             choosePath: function(index){
                 console.log(index);
@@ -102,9 +97,15 @@
                 this.getRead();
 
 
+            },
+            chooseFile: function(name){
+                    console.log(name);
+                this.$http.get('https://raw.githubusercontent.com/'+ this.fullRepoUrl +'/master/'+ name )
+                
             }
         
 	    },
+
 	     computed: {
 	        fullRepoUrl: function() {
 	            return this.username + '/' + this.repo;
@@ -140,10 +141,7 @@
              this.getFiles();
              this.getInfo();
              this.getRead();
-            }
-            
-
-        
+            } 
         }
     };
 
